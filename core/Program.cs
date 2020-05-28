86ac4a21886158f79395b75691afd9b8898f7bf3
aleksey86 <zheltov86@live.ru>
2020-05-28 19:14:59 +0300

    Added project
    new project

---
+++ core/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace core
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            Console.ReadLine();
+        }
+    }
+}
